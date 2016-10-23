f7e8d94a1d37a2f1b3bed9ad24b801a368a13bbe
Григорий Никониров <mrgbh007@gmail.com>
2016-10-23 18:16:56 +0700

    документирования модуля reportgenerator

--- reportgenerator/report.py
+++ reportgenerator/report.py
@@ -5,15 +5,21 @@
 #
 
 class ReportGenerator:
+	'''базовый класс отчета'''
 	
 	def __init__(self,name='report'):
+		'''конструктор принимает имя файла для отчета
+		открывает этот файл для записи'''
 		self.report=open(name,'w')
 		
-	def addText(self,text,end='\n'):
+	def addText(self,text):
+		'''добавляет текст в файл отчета'''
 		pass
 		
-	def addTable(self,table,end='\n'):
+	def addTable(self,table):
+		'''добавляет таблицу в файл отчета'''
 		pass
 		
 	def commitAndExit(self):
+		'''закрывает файл отчета'''
 		self.report.close()
